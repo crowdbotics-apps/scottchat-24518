dc7712db6d315603d3a4612fbccdb868934062cd
Crowdbotics <team@crowdbotics.com>
2021-02-14 09:33:22 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile205007', human_name: 'User Profile', access_route: 'UserProfile205007'},
 { name: 'Settings205006', human_name: 'Settings', access_route: 'Settings205006'},
 { name: 'Settings205004', human_name: 'Settings', access_route: 'Settings205004'},
 { name: 'SignIn2205002', human_name: 'Sign In 2', access_route: 'SignIn2205002'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile205007Navigator from '../features/UserProfile205007/navigator';
 import Settings205006Navigator from '../features/Settings205006/navigator';
 import Settings205004Navigator from '../features/Settings205004/navigator';
 import SignIn2205002Navigator from '../features/SignIn2205002/navigator';
@@ -16,6 +17,7 @@ import SignIn2205002Navigator from '../features/SignIn2205002/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+UserProfile205007: { screen: UserProfile205007Navigator },
 Settings205006: { screen: Settings205006Navigator },
 Settings205004: { screen: Settings205004Navigator },
 SignIn2205002: { screen: SignIn2205002Navigator },
